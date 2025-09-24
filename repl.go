87dcc30c615f0278c9e29f907cada6b9c54aed14
NZO-GB <enzogbrito2@gmail.com>
2025-09-24 12:46:34 +0100

    startRepl() added

--- repl.go
+++ repl.go
@@ -1,10 +1,29 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
 
-
 func cleanInput(text string) []string {
-	return strings.Fields(text)
+	return strings.Fields(strings.ToLower(text))
+}
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+		for {
+			fmt.Print("Pokedex > ")
+			scanner.Scan()
+
+			words := cleanInput(scanner.Text())
+			if len(words) == 0 {
+				continue
+			}
+
+			command := words[0]
+
+			fmt.Println("Your command was:", command)
+		}
 }
